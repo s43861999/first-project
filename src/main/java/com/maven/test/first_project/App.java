fd949de426356e82426c0a8dcd178d58ce67bd35
s43861999 <schouksey4@gmail.com>
2025-07-19 03:49:44 +0530

    update all after war generate

--- src/main/java/com/maven/test/first_project/App.java
+++ src/main/java/com/maven/test/first_project/App.java
@@ -3,6 +3,7 @@ package com.maven.test.first_project;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println("Learning devops class");
     
   }
 }
