aae5a2bf976237a350871a77b052f137ccc47088
s43861999 <schouksey4@gmail.com>
2025-10-11 12:05:34 +0530

    updated new code

--- src/main/java/com/maven/test/first_project/App.java
+++ src/main/java/com/maven/test/first_project/App.java
@@ -2,7 +2,7 @@ package com.maven.test.first_project;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("Hello World I am learning devops !");
     System.out.println("Learning devops class");
     
   }
